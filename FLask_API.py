fa678011080a42253c44bdc93ae3eefcf84b8a36
Nabendu Karmakar <nabendu@haptik.co>
2020-04-28 17:55:50 +0530

    Update FLask_API.py

--- FLask_API.py
+++ FLask_API.py
@@ -22,9 +22,23 @@ class QOS(Resource):
         return {
             "QOS": "96.9"
         }
-        
+
+    
+class QOSHistory(Resource):
+    def __init__(self):
+        pass
+    def get(self):
+        return {
+            "QOS-History": {
+                "Jan": "99.0",
+                "Feb": "98.7",
+                "Mar": "97.7"
+            }
+        }
+
 api.add_resource(HelloWorld,'/')
 api.add_resource(QOS,'/qos')
+api.add_resource(QOSHistory,'/qos-history')
 
 if __name__ == "__main__":
     app.run(debug=True)
