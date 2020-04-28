fd69ace1df3f90448387ade85e59df01c4bc027c
pranit1617 <51417647+pranit1617@users.noreply.github.com>
2020-04-28 20:38:53 +0530

    Update FLask_API.py

--- FLask_API.py
+++ FLask_API.py
@@ -14,31 +14,90 @@ class HelloWorld(Resource):
                 "Hello":"World"
             }
 
-
 class QOS(Resource):
     def __init__(self):
         pass
     def get(self):
-        return {
-            "QOS": "96.9"
+        return{
+            "QOS Score":"96.6"
         }
 
-    
-class QOSHistory(Resource):
+class QOShistory(Resource):
     def __init__(self):
         pass
     def get(self):
-        return {
-            "QOS-History": {
-                "Jan": "99.0",
-                "Feb": "98.7",
-                "Mar": "97.7"
+        return{
+            "QOS History":
+            {
+                "Vaseline":
+               [{
+                    "Jan":0.85,
+                    "Feb":0.86,
+                    "March":0.87,
+                    "April":0.875,
+                    "May":0.9,
+                    "June":0.89,
+                    "July":0.88,
+                    "August":0.89,
+                    "September":0.9,
+                    "November":0.93,
+                    "December":0.94
+                }],
+                
+                "Head & Shoulders":
+                {
+                    "Jan":"0.75",
+                    "Feb":"0.77",
+                    "March":"0.77",
+                    "April":"0.775",
+                    "May":"0.8",
+                    "June":"0.89",
+                    "July":"0.88",
+                    "August":"0.9",
+                    "September":"0.91",
+                    "November":"0.92",
+                    "December":"0.94"
+                }
             }
         }
 
+class DQ_issues_to_resolve(Resource):
+    def __init__(self):
+        pass
+    def get(self):
+        return{
+            "Sensor":"Columns count PRG"
+            [{
+                "ES":"100%",
+                "IT":"9%",
+                "UK":"4%",
+                "TOtal":"38%"
+            }],
+
+            "Sensor":"Columns count SPO"
+            [{
+                "ES":"100%",
+                "IT":"9%",
+                "UK":"4%",
+                "TOtal":"38%"
+            }],
+
+            "Sensor":"Null numbers(Age)"
+            [{
+                "ES":"100%",
+                "IT":"55%",
+                "UK":"100%",
+                "TOtal":"85%"
+            }]
+        }
+
+
+
+
 api.add_resource(HelloWorld,'/')
-api.add_resource(QOS,'/qos')
-api.add_resource(QOSHistory,'/qos-history')
+api.add_resource(QOS,'/QOS')
+api.add_resource(QOShistory,'/QOShistory')
+api.add_resource(DQ_issues_to_resolve,'/DQ_issue_reolve')
 
 if __name__ == "__main__":
     app.run(debug=True)
