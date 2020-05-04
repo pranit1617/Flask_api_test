3aaf857fb7394b78db84bc053e33071a1c2c51c6
Nabendu Karmakar <nabendu.karmakar@outlook.com>
2020-05-04 11:53:25 +0530

    Randomized the output values

--- FLask_API.py
+++ FLask_API.py
@@ -1,103 +1,112 @@
 from flask import Flask
-from flask_restful import Resource,Api
+from flask_restful import Resource, Api
+from random import randrange
 
 app = Flask(__name__)
 api = Api(app)
 
+
+def get_random_str(low=80, high=100):
+    return str(randrange(low, high, 2))
+
+
 class HelloWorld(Resource):
     def __init__(self):
         pass
-    
+
     def get(self):
-        
-            return {
-                "Hello":"World"
-            }
+        return {
+            "Hello": "World"
+        }
+
 
 class QOS(Resource):
     def __init__(self):
         pass
+
     def get(self):
         return{
-            "QOS Score":"96.6"
+            "QOS Score": get_random_str()
         }
+
 
 class QOShistory(Resource):
     def __init__(self):
         pass
+
     def get(self):
-        return{
+        return {
             "QOS History":
             {
                 "Vaseline":
-               [{
-                    "Jan":0.85,
-                    "Feb":0.86,
-                    "March":0.87,
-                    "April":0.875,
-                    "May":0.9,
-                    "June":0.89,
-                    "July":0.88,
-                    "August":0.89,
-                    "September":0.9,
-                    "November":0.93,
-                    "December":0.94
+                [{
+                    "Jan": get_random_str(low=60),
+                    "Feb": get_random_str(low=60),
+                    "March": get_random_str(low=60),
+                    "April": get_random_str(low=60),
+                    "May": get_random_str(low=60),
+                    "June": get_random_str(low=60),
+                    "July": get_random_str(low=60),
+                    "August": get_random_str(low=60),
+                    "September": get_random_str(low=60),
+                    "November": get_random_str(low=60),
+                    "December": get_random_str(low=60)
                 }],
-                
+
                 "Head & Shoulders":
                 {
-                    "Jan":"0.75",
-                    "Feb":"0.77",
-                    "March":"0.77",
-                    "April":"0.775",
-                    "May":"0.8",
-                    "June":"0.89",
-                    "July":"0.88",
-                    "August":"0.9",
-                    "September":"0.91",
-                    "November":"0.92",
-                    "December":"0.94"
+                    "Jan": get_random_str(low=60),
+                    "Feb": get_random_str(low=60),
+                    "March": get_random_str(low=60),
+                    "April": get_random_str(low=60),
+                    "May": get_random_str(low=60),
+                    "June": get_random_str(low=60),
+                    "July": get_random_str(low=60),
+                    "August": get_random_str(low=60),
+                    "September": get_random_str(low=60),
+                    "November": get_random_str(low=60),
+                    "December": get_random_str(low=60)
                 }
             }
         }
 
+
 class DQ_issues_to_resolve(Resource):
     def __init__(self):
         pass
+
     def get(self):
-        return{
+        return {
             "Columns count PRG":
             [{
-                "ES":"100%",
-                "IT":"9%",
-                "UK":"4%",
-                "TOtal":"38%"
+                "ES": get_random_str(low=60) + "%",
+                "IT": get_random_str(low=60) + "%",
+                "UK": get_random_str(low=60) + "%",
+                "TOtal": get_random_str(low=60) + "%"
             }],
 
             "Columns count SPO":
             [{
-                "ES":"100%",
-                "IT":"9%",
-                "UK":"4%",
-                "TOtal":"38%"
+                "ES": get_random_str(low=60) + "%",
+                "IT": get_random_str(low=60) + "%",
+                "UK": get_random_str(low=60) + "%",
+                "TOtal": get_random_str(low=60) + "%"
             }],
 
             "Null numbers(Age)":
             [{
-                "ES":"100%",
-                "IT":"55%",
-                "UK":"100%",
-                "TOtal":"85%"
+                "ES": get_random_str(low=60) + "%",
+                "IT": get_random_str(low=60) + "%",
+                "UK": get_random_str(low=60) + "%",
+                "TOtal": get_random_str(low=60) + "%"
             }]
         }
 
 
-
-
-api.add_resource(HelloWorld,'/')
-api.add_resource(QOS,'/QOS')
-api.add_resource(QOShistory,'/QOShistory')
-api.add_resource(DQ_issues_to_resolve,'/DQ_issue_reolve')
+api.add_resource(HelloWorld, '/')
+api.add_resource(QOS, '/QOS')
+api.add_resource(QOShistory, '/QOShistory')
+api.add_resource(DQ_issues_to_resolve, '/DQ_issue_reolve')
 
 if __name__ == "__main__":
     app.run(debug=True)
